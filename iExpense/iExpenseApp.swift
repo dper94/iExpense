8182b501a9c3d3c249b79ae84399e5988a69e5d6
Diego Perdomo <diegoperdomo94@gmail.com>
2023-07-01 10:44:57 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Diego Perdomo on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
